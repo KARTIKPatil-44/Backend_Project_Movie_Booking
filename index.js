5466cac4741660b26cc52f1bb411794194f0195a
KARTIK-44 <kartikpatilcs@gmail.com>
2026-02-06 23:16:41 +0530

    Feature: seeting up a basic server and database

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const Movie = require("./models/movie.model");
+
+env.config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/home", (req, res) => {
+  console.log("hitting the home");
+  return res.json({ success: true });
+});
+
+app.listen(process.env.PORT, async () => {
+  // this callback gets executed, once we successfully start the server on the given
+  console.log(`Server started on Port ${process.env.PORT}`);
+
+  try {
+    await mongoose.connect(process.env.DB_URL); // connected to the mongo server
+    console.log("Successfully connected to mongodb");
+    // await Movie.create({
+    //   name: "Interstaller",
+    //   description: "Science Friction",
+    //   casts: [
+    //     "Matthew McConaughey",
+    //     "Jessica Chastain",
+    //     "Anne Hathaway",
+    //     "Timothée Chalamet",
+    //   ],
+    //   director: "Cristopher Nolen",
+    //   trailerUrl: "https://youtu.be/zSWdZVtXT7E?si=r5P2fHwHJJ2tjYvF",
+    //   language: "English",
+    //   releaseDate: "18-03-2014",
+    //   releaseStatus: "RELEASED",
+    // });
+  } catch (err) {
+    console.log("Not able to connect to mongodb", err);
+  }
+});
