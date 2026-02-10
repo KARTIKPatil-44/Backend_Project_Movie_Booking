de2ef1e8cc9ca6d8ca01d0023f5ed1d2e05c2966
KARTIK-44 <kartikpatilcs@gmail.com>
2026-02-11 01:08:10 +0530

    Feat: Added delete movie api

--- Controllers/movie.controller.js
+++ Controllers/movie.controller.js
@@ -1,24 +1,55 @@
 const Movie = require("../models/movie.model");
 
+
 const createMovie = async (req, res) => {
   try {
     const movie = await Movie.create(req.body);
+
     return res.status(201).json({
       success: true,
       error: {},
       data: movie,
       message: "Successfully created a new movie",
     });
+
   } catch (err) {
     console.log(err);
     return res.status(500).json({
-      success: true,
+      success: false,
       error: err,
       data: {},
       message: "Somthing went wrong",
     });
   }
 };
+
+const deleteMovie = async (req, res) => {
+  try {
+    const response = await Movie.deleteOne({
+      _id: req.params.movieId,
+
+    });
+    return res.status(201).json({
+      success: true,
+      error: {},
+      message: "Successfully deleted the movie",
+      data: response,
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+      message: "Somthing went wrong",
+      data: {},
+    });
+  }
+};
+
+
+
+
 module.exports = {
   createMovie,
+  deleteMovie,
 };
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,5 +1,9 @@
 const MovieController = require("../Controllers/movie.controller");
 const routes = (app) => {
+  // POST REQUEST
   app.post("/mba/api/v1/movies", MovieController.createMovie);
+
+  // DELETE REQUEST
+  app.delete("/mba/api/v1/movies/:movieId", MovieController.deleteMovie);
 };
 module.exports = routes;
