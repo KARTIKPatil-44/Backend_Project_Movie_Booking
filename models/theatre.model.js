912b1a5c642f9482ff6a363c8fa68a2d136b034a
KARTIK-44 <kartikpatilcs@gmail.com>
2026-02-28 19:23:03 +0530

    Feat: Added reference to the movie collection inside theatre resource

--- models/theatre.model.js
+++ models/theatre.model.js
@@ -24,6 +24,10 @@ const theatreSchema = mongoose.Schema(
     },
 
     address: String,
+    movies: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Movie"
+    }
   },
   { timestamps: true },
 );
